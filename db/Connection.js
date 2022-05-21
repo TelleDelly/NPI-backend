72f4510f6726da3f1d717f62b694cbfbecba6abf
tellydelly <martinwilson450@gmail.com>
2022-05-21 11:33:06 -0700

    fixed .env access issue

--- db/Connection.js
+++ db/Connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 
-const mongoURI =
+const mongoURI = 
 process.env.NODE_ENV === 'production'
 ? process.env.DB_URL
 : process.env.DB_DEV_URL
 
-console.log(process.env.DB_URL)
 
 mongoose.connect(mongoURI)
 .then(instance => console.log(`Connected to: ${instance}`))
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const trailController = require('./controllers/trail-controller')
 const app = express()
 

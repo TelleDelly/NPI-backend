5294c27201e963680621a66da31ffceab7d6a9c0
tellydelly <martinwilson450@gmail.com>
2022-05-21 12:25:53 -0700

    tested delete create and get routes

--- controllers/trail-controller.js
+++ controllers/trail-controller.js
@@ -6,7 +6,30 @@ const Trail = require('../models/trail-model.js')
 
 trailRouter.get('/', (req, res) => {
     Trail.find({})
-    .then((trails) => res.json())
+    .then((trails) => res.send(trails))
+    .catch(console.error)
+})
+
+trailRouter.get('/:id', (req, res) => {
+    Trail.findById(req.params.id)
+    .then((trail) => res.send(trail))
+    .catch(console.error)
+})
+
+trailRouter.put('/:id', (req, res) => {
+    Trail.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .catch(console.error)
+})
+
+trailRouter.post('/', (req,res) => {
+    Trail.create(req.body)
+    .then(res.send("Added data"))
+    .catch(console.error)
+})
+
+trailRouter.delete('/:id', (req, res) => {
+    Trail.findByIdAndRemove(req.params.id)
+    .then(res.send("Deleted data"))
     .catch(console.error)
 })
 

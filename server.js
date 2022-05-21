13a9b668f6cd85154d99be2adb4714f1a474999f
tellydelly <martinwilson450@gmail.com>
2022-05-21 09:53:34 -0700

    created the controller to send data via fetch

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const trailController = require('./controllers/trail-controller')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use('/trails', trailController)
 
 const port = process.env.PORT || 4000
 

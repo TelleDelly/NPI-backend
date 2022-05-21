580664d69196831412a5aa68516f29ed54759024
tellydelly <martinwilson450@gmail.com>
2022-05-21 09:34:14 -0700

    created server and tested functionality

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})

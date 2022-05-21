08dd8c6148b45dbb3ae79f8989234e7cbbc90b7e
tellydelly <martinwilson450@gmail.com>
2022-05-21 09:29:28 -0700

    hiding api keys

---
+++ db/Connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const mongoURI =
+process.env.NODE_ENV === 'production'
+? process.env.DB_URL
+: process.env.DB_DEV_URL
+
+mongoose.connect(mongoURI)
+.then(instance => console.log(`Connected to: ${instance}`))
+.catch(console.error)
+
+module.exports = mongoose

f18340fb1c7096c9e8d0383844df87b32fc2bb42
tellydelly <martinwilson450@gmail.com>
2022-05-21 19:39:37 -0700

    testing connection

--- db/Connection.js
+++ db/Connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const mongoURI = 
-process.env.NODE_ENV === 'production'
-? process.env.DB_URL
-: process.env.DB_DEV_URL
+// const mongoURI = 
+// process.env.NODE_ENV === 'production'
+// ? process.env.DB_URL
+// : process.env.DB_DEV_URL
 
 
-mongoose.connect(mongoURI)
+// mongoose.connect(mongoURI)
+mongoose.connect('mongodb+srv://trailblazer_user:NXjP93JRz47sbcm7@cluster0.c3iwz.mongodb.net/NPI?retryWrites=true&w=majority')
 .then(instance => console.log(`Connected to: ${instance}`))
 .catch(console.error)
 

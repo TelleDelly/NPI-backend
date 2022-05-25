e612291f974b3dbf5d217128961663362e1fdc1b
tellydelly <martinwilson450@gmail.com>
2022-05-24 17:13:54 -0700

    enabled cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const parksapiController = require('./controllers/parksapiController')
 const trailController = require('./controllers/trail-controller')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.use('/trails', trailController)
 app.use('/parks', parksapiController)

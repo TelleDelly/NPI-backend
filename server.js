5b134e114f5a1aee57f8aec191eba696b3df7ec1
tellydelly <martinwilson450@gmail.com>
2022-05-25 17:00:13 -0700

    fixed port to be used for deployment and localhost testing

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use('/parks', parksapiController)
 const port = process.env.PORT || 4000
 
 
-app.listen(app.get("port"), () => {
+app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })

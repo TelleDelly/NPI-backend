80ddc957d5107c168a15fe54433547101f0f4fa6
tellydelly <martinwilson450@gmail.com>
2022-05-27 14:22:44 -0700

    formatting update for park-seeds.js

--- db/park-seeds.js
+++ db/park-seeds.js
@@ -1,12 +1,10 @@
 const Park = require('../models/park-model')
 const seedData = require('../models/parks.json')
 
-
 Park.deleteMany({})
 .then(() => {
     return Park.insertMany(seedData)
 })
-.then(console.log)
 .catch(console.error)
 .finally(() => {
     process.exit()

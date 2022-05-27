e0b9a59ba7ae60d8edaa1b0c8602d6918e702cf6
tellydelly <martinwilson450@gmail.com>
2022-05-27 14:32:19 -0700

    updated routes for park collection *added full CRUD

--- controllers/parksapiController.js
+++ controllers/parksapiController.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const { Router } = require('express')
 const parksRouter = express.Router()
-const parksData = require('../models/data.json')
+
+const Park = require('../models/park-model.js')
+
+//Access using the prefix /parks
 
 
-
-parksRouter.get('/home', (req, res) => {
-    res.send(parksData)
-    .then(console.log('Data is fetched on the backend'))
+parksRouter.get('/', (req, res) => {
+    Park.find({})
+    .then((parks) => res.send(parks))
+    .catch(console.error)
 })
 
-parksRouter.get('/home/activities/topics/:id', (req, res) => {
-    res.send(parksData)
-    console.log('Id data has been fetched')
+parksRouter.get('/:id', (req, res) => {
+    Park.findById(req.params.id)
+    .then((trail) => res.send(trail))
+    .catch(console.error)
 })
+
+parksRouter.put('/:id', (req, res) => {
+    Park.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(res.send('Updated data'))
+    .catch(console.error)
+})
+
+parksRouter.post('/', (req, res) => {
+    Park.create(req.body)
+    .then(res.send("Added data"))
+    .catch(console.error)
+})
+
+oarkRouter.delete('/:id', (req, res) => {
+    Park.findByIdAndRemove(req.params.id)
+    .then(res.send("deleted data"))
+    .catch(console.error)
+})
+
+module.exports = parksRouter
 
 module.exports = parksRouter
